629f3f17b0e155a24d89b1ab36f2aff5fa1a98e6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-25 19:16:02 -0500

    Exit code for recursive function; When 0-1 steps, only one way to reach top

--- stair_case.py
+++ stair_case.py
@@ -1,5 +1,15 @@
 print("Staircase!")
 
-# Function takes in amount of steps and returns number of ways to reach top
+# Recursive Function takes in amount of steps as parameters 
+# and returns number of ways to reach top
+# Takes in storage param. to store results and 
+# prevent redundant computation (Dynamic Programing)
 def stair_case(steps, storage):
+
+	# Exit code for recursive function
+	# When 0-1 steps, only one way to reach top
+	if (steps <= 1):
+		return 1
+
+
 
