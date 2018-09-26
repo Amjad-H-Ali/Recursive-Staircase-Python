37d7dbefbc783736b95e57c1e90643bb183cef3f
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-25 19:40:35 -0500

    Function calls and returns helper function so no need to pass in array

--- stair_case.py
+++ stair_case.py
@@ -4,7 +4,7 @@ print("Staircase!")
 # and returns number of ways to reach top
 # Takes in storage param. to store results and 
 # prevent redundant computation (Dynamic Programing)
-def stair_case(steps, storage):
+def helper(steps, storage):
 
 	# Exit code for recursive function
 	# When 0-1 steps, only one way to reach top
@@ -17,11 +17,30 @@ def stair_case(steps, storage):
 		return storage[steps]
 
 	# Initialize result variable
-	# Ex: 3 steps equals answer to stair_case(2) +  answer to stair_case(1)
+	# Ex: 3 steps equals answer to helper(2) +  answer to helper(1)
 	# Much like the Fibbonacci sequence.
-	result = stair_case(steps - 1, storage) + stair_case(steps - 2, storage)
+	result = helper(steps - 1, storage) + helper(steps - 2, storage)
 
 	# Return answer
 	return result
 
+# Function calls and returns helper function so no need to pass in array
+def stair_case(steps):
+	return helper(steps, [None] * (steps + 1))
 
+
+print(stair_case(0)) #1
+
+print(stair_case(1)) #1
+
+print(stair_case(2)) #2
+
+print(stair_case(3)) #3
+
+print(stair_case(4)) #5
+
+print(stair_case(5)) #8
+
+print(stair_case(6)) #13
+
+print(stair_case(7)) #21
