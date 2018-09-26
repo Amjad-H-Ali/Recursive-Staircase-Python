2d82b178fa18ed1a742a9b67f9e63cd47c79631b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-25 19:23:28 -0500

    Initialize result variable; Ex: 3 steps equals answer to stair_case(2) +  answer to stair_case(1); Much like the Fibbonacci sequence.

--- stair_case.py
+++ stair_case.py
@@ -14,7 +14,13 @@ def stair_case(steps, storage):
 	# If result was already found and stored,
 	# no need to repeat computation
 	if (storage[steps]):
-		return storage[steps] 	
+		return storage[steps]
+
+	# Initialize result variable
+	# Ex: 3 steps equals answer to stair_case(2) +  answer to stair_case(1)
+	# Much like the Fibbonacci sequence.
+	result = stair_case(steps - 1, storage) + stair_case(steps - 2, storage)
+		 	
 
 
 
