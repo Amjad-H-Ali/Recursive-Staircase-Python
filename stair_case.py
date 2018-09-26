3adf92f31ccd9c0a2326150a033c7659b9b1cfb4
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-25 19:24:21 -0500

    Function returns answer

--- stair_case.py
+++ stair_case.py
@@ -20,7 +20,8 @@ def stair_case(steps, storage):
 	# Ex: 3 steps equals answer to stair_case(2) +  answer to stair_case(1)
 	# Much like the Fibbonacci sequence.
 	result = stair_case(steps - 1, storage) + stair_case(steps - 2, storage)
-		 	
 
+	# Return answer
+	return result
 
 
